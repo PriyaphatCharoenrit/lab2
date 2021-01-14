5c3e946ea8e88f3614e6c0e2afa6ac1a46a93b14
DESKTOP-6V70R8A\DCOM863 <614234013@parichat.skru.ac.th>
2021-01-14 12:57:19 +0700

    14/1/2564

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,54 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <h1>{{ message}}</h1><br/>
+    <span v-text="message"></span>
+    <hr/>
+
+    <h1 v-show="isShow">Hello!</h1>
+    
+    <hr/>
+    <div v-bind:key="emp.id" v-for="emp in emplayees">
+        <span v-bind:class="(emp.Salary>90000)? 'high' : (emp.Salary>=30000)? 'medium' :'low' ">
+
+        {{ emp.name}}
+        </span>
+
+        <span v-if="emp.Salary>90000">***</span>
+        <span v-else-if="emp.Salary>=30000">**</span>
+        <span v-else>*</span>
+    </div>
+
+    <hr/>
+    <a v-bind:href="urlSKRU">SKRU</a>
+    <hr/>
+    <img v-bind:src="require('@/assets/' + pictureName +'.png')" width ="200px "/>
+
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+  },
+  data(){
+    return{
+      pictureName:"logo",
+      urlSKRU:"http://www.skru.ac.th",
+      message:"Hello world!",
+      isShow: false,
+      emplayees: [ 
+        {"id":1,"name":"john","Salary": 30000} , 
+        {"id":2,"name":"Trump","Salary": 10000} ,
+        {"id":3,"name":"Peter","Salary": 99000} 
+      ],
+    }
+  },
 }
 </script>
 
@@ -24,5 +60,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.high{
+  color: green;
+}
+.medium{
+  color: yellow;
+}
+.low{
+  color: red;
 }
 </style>
